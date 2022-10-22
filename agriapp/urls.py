2ffa4ba46259d2b5147229f59a7e1b0442270c22
krish5555 <ka36l1107@gmail.com>
2022-10-23 02:56:34 +0530

    added landing page

--- agriapp/urls.py
+++ agriapp/urls.py
@@ -6,5 +6,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home1.html"), name = "home"),
     path('login/', TemplateView.as_view(template_name="login.html"), name = "login"),
+    path('acess_denied/', TemplateView.as_view(template_name="acess_denied.html"), name = "acess_denied"),
+    path('c/', TemplateView.as_view(template_name="register.html"), name = "c"),
 ]
 
