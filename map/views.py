e1b84d99291ac16df7851115a53c37895b7cc286
krish5555 <ka36l1107@gmail.com>
2022-10-29 04:08:22 +0530

    added map

--- map/views.py
+++ map/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-import folium as f
+import folium as fol
+import geocoder as geo
 
 # Create your views here.
 
 def map_view(request):
-    # create map objects
-    map = f.Map()
+    location  = geo.osm('IN')
+    laitude   = location.lat
+    longitude = location.lng
+    country   = location.country
+    print(laitude,'----')
+    # Create map objects
+    map = fol.Map(location=[19, -12], zoom_start = 2)
+    fol.Marker([laitude, longitude], tooltip = 'Click for details', popup = f'<a href="#">{country}<a>').add_to(map)
+    # Htmp representation of teh map object
     map = map._repr_html_()
     context = {
         'map' : map
