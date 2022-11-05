10686810da1eeb357d9be12a3eb21a23c708e67d
krish5555 <ka36l1107@gmail.com>
2022-11-06 02:26:09 +0530

    register1 page added

--- agriapp/urls.py
+++ agriapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login1/', TemplateView.as_view(template_name="login1.html"), name = "login1"),
     path('acess_denied/', TemplateView.as_view(template_name="acess_denied.html"), name = "acess_denied"),
     path('register/', TemplateView.as_view(template_name="register.html"), name = "register"),
+    path('register-1/', TemplateView.as_view(template_name="register_1.html"), name = "register-1"),
     path('users/', TemplateView.as_view(template_name="users.html"), name = "users"),
     path('forgot_password/', TemplateView.as_view(template_name="forgot_password.html"), name = "forgot_password"),
 ]
